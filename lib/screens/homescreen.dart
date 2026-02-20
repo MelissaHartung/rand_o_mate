ce858d7a2e7b879ba28327299fe5858f4b0bc48f
MelissaHartung <Melissahartung99@gmail.com>
2026-02-20 20:55:56 +0100

    Add Homescreen and Zufallsrad widget implementation

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:rand_o_mate/screens/widgets/zufallsrad.dart';
+
+class Homescreen extends StatelessWidget{
+  const Homescreen({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      drawer: Drawer(
+        backgroundColor: Color.fromARGB(249, 34, 142, 20),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.food_bank_outlined),
+              title: Text('Rezepte'),
+            ),
+            ListTile(
+              leading: Icon(Icons.catching_pokemon_outlined),
+              title: Text('Aktivitäten'),
+            ),
+            ListTile(
+              leading: Icon(Icons.camera_alt_outlined),
+              title: Text('Filme'),
+            ),
+          ],
+        ),
+      ),
+       
+      body: const Center(
+        child: Zufallsrad(),
+      ),
+    );
+    }
+}
+
---
+++ lib/screens/widgets/zufallsrad.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+ class Zufallsrad extends StatelessWidget {
+  const Zufallsrad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+
+      
+    );
+  }
+}
